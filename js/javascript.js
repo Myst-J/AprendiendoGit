1c9dd53a045f29ffda2088d07de1712ef0dd54dd
JoseP <60764749+Myst-J@users.noreply.github.com>
2023-12-09 23:58:42 -0400

    Agregado función multiplicar

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,13 @@
 const sumarDosNumeros=function (num1,num2){
-return console.log(`La dsuma de los numeros es: ${(parseFloat(num1)+parseFloat(num2))} :D`)
+return console.log(`La suma de los numeros es: ${(parseFloat(num1)+parseFloat(num2))} :D`)
 
+}
+const tablaMultiplicar=function (numero){
+ for(let i=0;i<numero;i++){
+    console.log(`${numero}x${(i+1)}=${numero*(i+1)}`);
+
+   
+ }
+
+  
 }
